e1664098f6032fd04ca5f4cbffeb59a4f764dabc
Luke Davies <lkdvs3@gmail.com>
2018-03-18 21:36:40 +0000

    Re-added serialization attribute

--- Assets/Scripts/MainGame/Effects/Effect.cs
+++ Assets/Scripts/MainGame/Effects/Effect.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization;
 /// If an effect property is null, then it doesn't count to the overall
 /// value <see cref="T:EffectManager"/> returns.
 /// </summary>
+[Serializable]
 public abstract class Effect
 {
     #region Private Fields
