31dc8d5010e605e7f9f6d23eaec7d100f09dc535
Luke Davies <lkdvs3@gmail.com>
2018-04-18 14:07:51 +0100

    Added base card manager

---
+++ Assets/Scripts/MainGame/Effects/CardManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    #region Private Fields
+
+    List<CardController> _cards;
+
+    #endregion
+}
