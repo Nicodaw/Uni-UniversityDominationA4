7bea63be9b8d75a3e1f74c15f9d238f8b8b7e929
Luke Davies <lkdvs3@gmail.com>
2018-04-30 19:18:59 +0100

    Added GUI controller to maintain size of GUI

---
+++ Assets/Scripts/MainGame/GuiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiController : MonoBehaviour
+{
+    #region Unity Bindings
+
+    [SerializeField] RectTransform _canvasRectTransform;
+
+    #endregion
+
+    #region Private Fields
+
+    Camera _mainCamera;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Bounds cameraBounds = _mainCamera.OrthographicBounds();
+        _canvasRectTransform.sizeDelta = new Vector2(
+            cameraBounds.size.x / transform.localScale.x,
+            cameraBounds.size.z / transform.localScale.y);
+    }
+
+    #endregion
+}
