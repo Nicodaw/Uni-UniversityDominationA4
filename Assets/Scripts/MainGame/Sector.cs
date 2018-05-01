39f9ef6daeb213fe5754303d8848c8c63009cf3b
A. Simeonov <angel.st.sim@gmail.com>
2018-05-01 17:33:35 +0100

    Implemented sector emission for #26

--- Assets/Scripts/MainGame/Sector.cs
+++ Assets/Scripts/MainGame/Sector.cs
@@ -16,6 +16,8 @@ public class Sector : MonoBehaviour
     #region Private fields
 
     const float DefaultHighlightAmount = 0.2f;
+    readonly Color EmissionBaseValue = Color.black;
+    readonly Color EmissionActiveValue = new Color(0.25f, 0.25f, 0.25f);
 
     int _id;
     Transform _unitStore;
@@ -123,19 +125,25 @@ public class Sector : MonoBehaviour
         get { return _highlighed; }
         set
         {
-            if (value != _highlighed)
-            {
-                Renderer rend = GetComponent<Renderer>();
-                Color currentColor = rend.material.color;
-                Color offset = new Color(DefaultHighlightAmount, DefaultHighlightAmount, DefaultHighlightAmount);
-                Color newColor;
-                if (value)
-                    newColor = currentColor + offset;
-                else
-                    newColor = currentColor - offset;
-                rend.material.color = newColor;
-            }
+
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", value ? EmissionActiveValue : EmissionBaseValue);
             _highlighed = value;
+
+
+
+            //if (value != _highlighed)
+            //{
+            //    Renderer rend = GetComponent<Renderer>();
+            //    Color currentColor = rend.material.color;
+            //    Color offset = new Color(DefaultHighlightAmount, DefaultHighlightAmount, DefaultHighlightAmount);
+            //    Color newColor;
+            //    if (value)
+            //        newColor = currentColor + offset;
+            //    else
+            //        newColor = currentColor - offset;
+            //    rend.material.color = newColor;
+            //}
+            //_highlighed = value;
         }
     }
 
